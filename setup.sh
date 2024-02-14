381f250ea4bf457158595cdca56de30941b0038e
Kaiyuan Tan <kt19@illinois.edu>
2024-02-14 16:18:15 -0600

    update install requirements

--- setup.sh
+++ setup.sh
@@ -1,23 +1,15 @@
 
 #CUDA version = 12.1
 
-# conda create -n xmf python=3.8 -y 
-# conda activate xmf
-# conda install pytorch==1.10.2 torchvision==0.11.3 cudatoolkit=10.2 -c pytorch -y 
+apt install libpcl-dev -y
 conda install -c fvcore -c iopath -c conda-forge fvcore iopath
 conda install pytorch3d -c pytorch3d
-
+ 
 pip install -r requirements.txt
-# pip install setuptools==59.5.0
-
-#now compile all the 3rd party modules in /decoder/utils/
 
 cd decoder/utils/furthestPointSampling
 python3 setup.py install
 
-# https://github.com/stevenygd/PointFlow/tree/master/metrics --> not used
-cd decoder/utils/metrics/pytorch_structural_losses
-make
 
 # https://github.com/sshaoshuai/Pointnet2.PyTorch
 cd decoder/utils/Pointnet2.PyTorch/pointnet2
@@ -30,3 +22,6 @@ python3 setup.py install
 # used to generate partialized partial inputs
 cd decoder/utils/randPartial
 python3 setup.py install
+
+# for some reason cc1plus is missing from conda env without installing the following
+conda install -c conda-forge gxx_linux-64
