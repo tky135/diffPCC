f870c94d4fcb6126584c59075da07d7cbfbe2378
KT <kt19@illinois.edu>
2024-02-17 19:51:56 +0000

    modify setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@
 
 apt install libpcl-dev -y
 conda install -c fvcore -c iopath -c conda-forge fvcore iopath
-conda install pytorch3d -c pytorch3d
- 
+# conda install pytorch3d -c pytorch3d
+pip install --no-index --no-cache-dir pytorch3d -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py310_cu121_pyt210/download.html
+
 pip install -r requirements.txt
 
 cd decoder/utils/furthestPointSampling
@@ -12,18 +13,18 @@ pip install .
 
 
 # https://github.com/sshaoshuai/Pointnet2.PyTorch
-cd decoder/utils/Pointnet2.PyTorch/pointnet2
+cd ../Pointnet2.PyTorch/pointnet2
 pip install .
 
 # https://github.com/daerduoCarey/PyTorchEMD
-cd decoder/utils/PyTorchEMD
+cd ../../PyTorchEMD
 pip install .
 
 # used to generate partialized partial inputs
-cd decoder/utils/randPartial
+cd ../randPartial
 pip install .
 
-cd decoder/utils/torch-batch-svd
+cd ../torch-batch-svd
 pip install .
 
 # for some reason cc1plus is missing from conda env without installing the following
